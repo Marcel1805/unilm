edf83652d4d9889c99feb2009fde3b71235fab0d
Marci <marci_xd@yahoo.de>
2022-10-12 20:48:33 +0200

    args gpu

--- dit/classification/utils.py
+++ dit/classification/utils.py
@@ -276,7 +276,7 @@ def init_distributed_mode(args):
         return
 
     args.distributed = True
-
+    print("das ist args.gpu", args.gpu)
     torch.cuda.set_device(args.gpu)
     args.dist_backend = 'nccl'
     print('| distributed init (rank {}): {}, gpu {}'.format(
